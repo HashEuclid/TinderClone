1c0c4c2827a71d0a7b7f123f6d9ee2c1e280bac2
max0099 <malikvishant93@gmail.com>
2025-06-12 13:47:52 +0530

    Get APIs

--- src/app.js
+++ src/app.js
@@ -18,6 +18,38 @@ app.post("/signup", async (req, res) => {
   }
 });
 
+//  GET user by email
+app.get("/user", async (req, res) => {
+  try {
+    const user = await User.findOne({emailId: req.body.emailId});
+    if(!user) {
+      res.status(404).send("User not found");
+    } else {
+      res.send(user);
+    }
+    // const users = await User.find({ emailId: req.body.emailId });
+    // if (users.length === 0) {
+    //   res.status(404).send("User not found");
+    // } else {
+    //   res.send(users);
+    // }
+  } catch (err) {
+    res.status(400).send("Something went wrong");
+  }
+});
+
+// FEED API - GET /feed - get all the users from the database
+
+app.get("/feed", async(req, res) => {
+  try{
+    const users = await User.find({});
+    res.send(users);
+  }
+  catch(err) {
+    res.status(400).send("Something went wrong");
+  }
+});
+
 connectDB()
   .then(() => {
     console.log("Cluster connection established....");
