772c4d93d83e2bcc1b0743a2ea7e1f68135b595a
max0099 <malikvishant93@gmail.com>
2025-07-11 21:12:12 +0530

    Building feed API and pagination

--- src/homework.md
+++ src/homework.md
@@ -136,5 +136,20 @@
 - Read more about ref and populate https://mongoosejs.com/docs/populate.html
 - Create GET /user/requests/received with all the checks
 - Create GET /user/connections.
-- 
+- Logic for GET /feed API
+- Explore the $nin, $and, and $ne and others query operators - comparison queries
+- Pagination
+
+/feed?page=1&limit=10 => 1-10  => .skip(0) & .limit(10)
+
+/feed?page=2&limit=10 => 11-20 => .skip(10) & .limit(10)
+
+/feed?page=3&limit=10 => 21-30 => .skip(20) & .limit(10)
+
+    skip = (page-1)*limit // formula for skip
+
+.skip() - how many documents do you skip from the starting?
+.limit() - how many documents do you want?
+
+
 -->  
--- src/routes/user.js
+++ src/routes/user.js
@@ -2,6 +2,7 @@ const express = require("express");
 const { userAuth } = require("../middlewares/auth");
 const ConnectionRequestModel = require("../models/connectionRequest");
 const userRouter = express.Router();
+const User = require("../models/user");
 
 const USER_SAFE_DATA = "firstName lastName age skills";
 
@@ -31,18 +32,63 @@ userRouter.get("user/connections", userAuth, async (req, res) => {
         { toUserId: loggedInUser._id, status: "accepted" },
         { fromUserId: loggedInUser._id, status: "accepted" },
       ],
-    }).populate("fromUserId",USER_SAFE_DATA).populate("toUserId",USER_SAFE_DATA);
+    })
+      .populate("fromUserId", USER_SAFE_DATA)
+      .populate("toUserId", USER_SAFE_DATA);
 
     const data = connectionRequest.map((row) => {
-        if(row.fromUserId._id.toString() ==  loggedInUser._id.toString()) {
-           return row.toUserId;
-        }
-        return row.fromUserId;
+      if (row.fromUserId._id.toString() == loggedInUser._id.toString()) {
+        return row.toUserId;
+      }
+      return row.fromUserId;
     });
 
-    res.json({data: data});
+    res.json({ data: data });
   } catch (err) {
     res.status(400).send("ERROR: " + err.message);
+  }
+});
+
+userRouter.get("/user/feed", userAuth, async (req, res) => {
+  try {
+    // User should see all the user cards except
+    // 1. his own cards
+    // 2. User's connections
+    // 3. ignored people
+    // 4. Already sent the connection request
+
+    const loggedInUser = req.user;
+
+    const page = parseInt(req.query.page) || 1;
+    let limit = parseInt(req.query.limit) || 10;
+    limit = limit > 50 ? 50 : limit;
+
+    const skip = (page - 1) * limit;
+
+    //  find all the connection request that i have snt or received
+    const connectionRequest = await ConnectionRequest.find({
+      $or: [{ fromUserId: loggedInUser._id }, { toUserId: loggedInUser._id }],
+    }).select("fromUserId toUserId");
+
+    const hideUsersFromFeed = new Set();
+    connectionRequest.forEach((req) => {
+      hideUsersFromFeed.add(req.fromUserId.toString());
+      hideUsersFromFeed.add(req.toUserId.toString());
+    });
+
+    const users = await User.find({
+      $and: [
+        { _id: { $ne: loggedInUser._id } },
+        { _id: { $nin: Array.from(hideUsersFromFeed) } },
+      ],
+    })
+      .select(USER_SAFE_DATA)
+      .skip(skip)
+      .limit(limit);
+
+    res.json({ data: users });
+  } catch (err) {
+    res.status(400).json({ message: err.message });
   }
 });
 
